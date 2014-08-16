807338cc84e80b8e98c246a9d9e9dae09fd8f107
Fanda <Fanda.email.addres.not.set.yet.intentionally@reason.I.am.not.sure.which.email.address.to.set>
2014-08-16 22:00:14 +0200

    A lot of progress
    Create function test_last_command_success_or_die_with_custom_exit_code().
    
    Create the basic skeleton, basic functionality, several main features.
    i.e.
    1) exit with non-zero exit code if an error encountered
    2) mkdir NewBackupDirNamedWithDateAndNumber,
    3) create tar bz2-compressed archive in the created dir
    
    It does not handle command line arguments.
    It has preset dir to be archived.
    The preset dir is the config dir of chromium in $HOME.
    
    This version has bash syntax errors and does not work.
    
    Originally this was the file
    /home/kdokoli/Dokumenty/fanda/after-2014-03-16/after-2014-07-03/1.unsorted/6.backup-script-tar-bz2.2.sh
    
     # On branch record-original-older-untracked-files
     # Changes to be committed:
     #	modified:   backup-script-tar-bz2.sh
     #

--- backup-script-tar-bz2.sh
+++ backup-script-tar-bz2.sh
@@ -1,1 +1,72 @@
-tar cvfj chromium.tar.bz2 /home/kdokoli/.config/chromium
+# Fanda kdokoli@doma 2014-07-08
+
+
+#----------- section: define variables
+
+TAR_ARCHIVE_FILENAME_BODY_F="chromium"
+DATE_F=$(date +'%F')
+TIME_F=$(date +'%H-%M-%S_%Z')
+TAR_ARCHIVE_EXTENSION_F="tar.bz2"
+TAR_ARCHIVE_FILENAME_F="${TAR_ARCHIVE_FILENAME_BODY_F}.${DATE_F}_${TIME_F}.${TAR_ARCHIVE_EXTENSION_F}"
+SOURCE_PATH_F="/home/kdokoli/.config/chromium/"
+TARGET_BACKUP_DIR_ROOT_F="/home/kdokoli/backup/doma/after-2014-07-07"
+TARGET_BACKUP_DIR_F=""
+
+
+
+function test_last_command_success_or_die_with_custom_exit_code() {
+	if [ ! "${1}" -eq "0" ]
+	then
+		exit("${2}")
+	fi
+	return 0
+}
+
+
+
+
+
+
+
+
+#----------- section: set and test variables and funtions
+
+# set a yet nonexistent directory path
+N_F=1
+while [ -d "TARGET_BACKUP_DIR_ROOT_F/${DATE_F}.${N_F}" ]
+do
+	N_F=$(echo "${N_F} + 1" | bc)
+done
+
+# test if an integer
+if [ ${N_F} =~ '^[0-9]+$' ]
+then
+	TARGET_BACKUP_DIR_F="${TARGET_BACKUP_DIR_ROOT_F}/${DATE_F}.${N_F}"
+else
+	exit(11)
+fi
+
+# test if a target directory name is ok
+if [ -n "${TARGET_BACKUP_DIR_F}" -a ! -d "${TARGET_BACKUP_DIR_F}" ]
+then
+	exit(12)
+fi
+
+# set the final path
+TARGET_PATH_F="${TARGET_BACKUP_DIR_F}/${TAR_ARCHIVE_FILENAME_F}"
+
+
+
+
+#----------- section: do commands
+
+
+mkdir --verbose "${TARGET_BACKUP_DIR_F}"
+test_last_command_success_or_die_with_custom_exit_code(${?}, 13)
+cd "${TARGET_BACKUP_DIR_F}"
+test_last_command_success_or_die_with_custom_exit_code(${?}, 14)
+tar cvfj "${SOURCE_PATH_F}" "${TARGET_PATH_F}"
+test_last_command_success_or_die_with_custom_exit_code(${?}, 15)
+
+
+exit(0)
