dee46bbabeee255283e286f00f7cf9b4502f296d
Fanda <Fanda.email.addres.not.set.yet.intentionally@reason.I.am.not.sure.which.email.address.to.set>
2014-08-17 16:21:54 +0200

    Add command line arguments handling
    Just few simple changes:
    
    1. Add simple handling of 2 required positional command line arguments.
    
    2. Add simple test to test if the script has syntax errors inside itself.
    
    3. Echo simple custom error message to stdout in the function
    test_last_command_success_or_die_with_custom_exit_code().
    
    4. Several other minor changes.
    
     # On branch record-original-older-untracked-files
     # Changes to be committed:
     #	modified:   backup-script-tar-bz2.sh
     #

--- backup-script-tar-bz2.sh
+++ backup-script-tar-bz2.sh
@@ -3,12 +3,13 @@
 
 #----------- section: define variables
 
-TAR_ARCHIVE_FILENAME_BODY_F="chromium"
+THIS_SCRIPT_FILENAME_F="$0"
+TAR_ARCHIVE_FILENAME_BODY_F="${2}"
 DATE_F=$(date +'%F')
 TIME_F=$(date +'%H-%M-%S_%Z')
 TAR_ARCHIVE_EXTENSION_F="tar.bz2"
 TAR_ARCHIVE_FILENAME_F="${TAR_ARCHIVE_FILENAME_BODY_F}.${DATE_F}_${TIME_F}.${TAR_ARCHIVE_EXTENSION_F}"
-SOURCE_PATH_F="/home/kdokoli/.config/chromium/"
+SOURCE_PATH_F="${1}"
 TARGET_BACKUP_DIR_ROOT_F="/home/kdokoli/backup/doma/after-2014-07-07"
 TARGET_BACKUP_DIR_F=""
 
@@ -17,7 +18,8 @@ TARGET_BACKUP_DIR_F=""
 function test_last_command_success_or_die_with_custom_exit_code() {
 	if [ ! "${1}" -eq "0" ]
 	then
-		exit("${2}")
+		echo "Error. Script ${THIS_SCRIPT_FILENAME_F} says: Error, Exit code: ${2}"
+		exit "${2}"
 	fi
 	return 0
 }
@@ -31,6 +33,10 @@ function test_last_command_success_or_die_with_custom_exit_code() {
 
 #----------- section: set and test variables and funtions
 
+# test if this script has syntax errors in it
+bash -n "$0"
+test_last_command_success_or_die_with_custom_exit_code "$?" "10"
+
 # set a yet nonexistent directory path
 N_F=1
 while [ -d "TARGET_BACKUP_DIR_ROOT_F/${DATE_F}.${N_F}" ]
@@ -43,13 +49,13 @@ if [ ${N_F} =~ '^[0-9]+$' ]
 then
 	TARGET_BACKUP_DIR_F="${TARGET_BACKUP_DIR_ROOT_F}/${DATE_F}.${N_F}"
 else
-	exit(11)
+	exit 11
 fi
 
 # test if a target directory name is ok
 if [ -n "${TARGET_BACKUP_DIR_F}" -a ! -d "${TARGET_BACKUP_DIR_F}" ]
 then
-	exit(12)
+	exit 12
 fi
 
 # set the final path
@@ -62,11 +68,11 @@ TARGET_PATH_F="${TARGET_BACKUP_DIR_F}/${TAR_ARCHIVE_FILENAME_F}"
 
 
 mkdir --verbose "${TARGET_BACKUP_DIR_F}"
-test_last_command_success_or_die_with_custom_exit_code(${?}, 13)
+test_last_command_success_or_die_with_custom_exit_code "$?" "13"
 cd "${TARGET_BACKUP_DIR_F}"
-test_last_command_success_or_die_with_custom_exit_code(${?}, 14)
+test_last_command_success_or_die_with_custom_exit_code "$?" "14"
 tar cvfj "${SOURCE_PATH_F}" "${TARGET_PATH_F}"
-test_last_command_success_or_die_with_custom_exit_code(${?}, 15)
+test_last_command_success_or_die_with_custom_exit_code "$?" "15"
 
 
-exit(0)
+exit 0
