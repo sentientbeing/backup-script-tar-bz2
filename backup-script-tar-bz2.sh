2d88e1cb7db7f519aa12f0f64bc76a8f375665ec
Fanda <Fanda.email.addres.not.set.yet.intentionally@reason.I.am.not.sure.which.email.address.to.set>
2014-08-17 19:18:31 +0200

    Add parameter test
    Minor changes:
    
    1. Add test to test a correct number of parameters.
    
    2. Create function exit_with_custom_message_and_code().
    
    3. Add tests to test correctness target backup directory name.
    
    4. Several other minor changes.
    
     # On branch record-original-older-untracked-files
     # Changes to be committed:
     #	modified:   backup-script-tar-bz2.sh
     #

--- backup-script-tar-bz2.sh
+++ backup-script-tar-bz2.sh
@@ -1,9 +1,51 @@
 # Fanda kdokoli@doma 2014-07-08
+# backup script
+# 2 positional parameters
+
+
+#----------- section: define essential functions, defining means no harm no command run:
+
+
+function exit_with_custom_message_and_code() {
+	echo "Error. Script ${THIS_SCRIPT_FILENAME_F} says: Error, Exit code: ${1}"
+	exit "${1}"
+}
+
+
+
+function test_last_command_success_or_die_with_custom_exit_code() {
+	if [ "${1}" -ne "0" ]
+	then
+		exit_with_custom_message_and_code "${2}"
+	fi
+	return 0
+}
+
+#----------- section: essential test: test if this script has syntax errors in it
+
+
+# test if this script has syntax errors in it
+bash -n "${0}"
+# test the last command (bash -n "${0}") if this script has syntax errors in it
+test_last_command_success_or_die_with_custom_exit_code "${?}" "10"
+
+
+#----------- section: essential test: test if this script was called with the correct number of parameters
+
+
+# test if this script was called with the correct number of parameters
+if [ "${#}" -ne "2" ]
+then
+	TMP_EXIT_CODE_1_F="15"
+	echo "Error. Script ${0} says: Error, Exit code: ${TMP_EXIT_CODE_1_F}"
+	exit "${TMP_EXIT_CODE_1_F}"
+fi
+
 
 
 #----------- section: define variables
 
-THIS_SCRIPT_FILENAME_F="$0"
+THIS_SCRIPT_FILENAME_F="${0}"
 TAR_ARCHIVE_FILENAME_BODY_F="${2}"
 DATE_F=$(date +'%F')
 TIME_F=$(date +'%H-%M-%S_%Z')
@@ -15,14 +57,6 @@ TARGET_BACKUP_DIR_F=""
 
 
 
-function test_last_command_success_or_die_with_custom_exit_code() {
-	if [ ! "${1}" -eq "0" ]
-	then
-		echo "Error. Script ${THIS_SCRIPT_FILENAME_F} says: Error, Exit code: ${2}"
-		exit "${2}"
-	fi
-	return 0
-}
 
 
 
@@ -33,29 +67,34 @@ function test_last_command_success_or_die_with_custom_exit_code() {
 
 #----------- section: set and test variables and funtions
 
-# test if this script has syntax errors in it
-bash -n "$0"
-test_last_command_success_or_die_with_custom_exit_code "$?" "10"
 
 # set a yet nonexistent directory path
+#                  steps: 1. 2. ...
+# 1. set a yet nonexistent directory path
 N_F=1
 while [ -d "TARGET_BACKUP_DIR_ROOT_F/${DATE_F}.${N_F}" ]
 do
 	N_F=$(echo "${N_F} + 1" | bc)
 done
 
-# test if an integer
-if [ ${N_F} =~ '^[0-9]+$' ]
+# 2. test if an integer
+if [ "${N_F}" =~ "^[0-9]+$" ]
 then
 	TARGET_BACKUP_DIR_F="${TARGET_BACKUP_DIR_ROOT_F}/${DATE_F}.${N_F}"
 else
-	exit 11
+	exit_with_custom_message_and_code "20"
 fi
 
-# test if a target directory name is ok
-if [ -n "${TARGET_BACKUP_DIR_F}" -a ! -d "${TARGET_BACKUP_DIR_F}" ]
+# 3. test if a target directory name is ok
+if [ -n "${TARGET_BACKUP_DIR_F}" ]
 then
-	exit 12
+	exit_with_custom_message_and_code "25"
+fi
+
+# 4. test if a target directory exists and terminate if it does
+if [ ! -d "${TARGET_BACKUP_DIR_F}" ]
+then
+	exit_with_custom_message_and_code "25"
 fi
 
 # set the final path
@@ -68,11 +107,12 @@ TARGET_PATH_F="${TARGET_BACKUP_DIR_F}/${TAR_ARCHIVE_FILENAME_F}"
 
 
 mkdir --verbose "${TARGET_BACKUP_DIR_F}"
-test_last_command_success_or_die_with_custom_exit_code "$?" "13"
+test_last_command_success_or_die_with_custom_exit_code "${?}" "30"
 cd "${TARGET_BACKUP_DIR_F}"
-test_last_command_success_or_die_with_custom_exit_code "$?" "14"
+test_last_command_success_or_die_with_custom_exit_code "${?}" "35"
 tar cvfj "${SOURCE_PATH_F}" "${TARGET_PATH_F}"
-test_last_command_success_or_die_with_custom_exit_code "$?" "15"
-
+test_last_command_success_or_die_with_custom_exit_code "${?}" "40"
+ls -l "${TARGET_PATH_F}"
+test_last_command_success_or_die_with_custom_exit_code "${?}" "45"
 
 exit 0
